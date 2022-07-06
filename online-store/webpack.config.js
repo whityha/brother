3d9d513f7f6d6da5472ea4988fac4361119d86a5
Pavel <2042_2042@mail.ru>
2022-07-06 20:14:09 +0300

    feat: added configs, .sass/.html files for the app development

--- online-store/webpack.config.js
+++ online-store/webpack.config.js
@@ -1,9 +1,53 @@
 const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.ts',
-  output: {
-    filename: 'main.ts',
-    path: path.resolve(__dirname, 'dist'),
+module.exports = baseConfig = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  mode: 'production',
+  module: {
+    rules: [
+        {
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+        {
+            test: /\.(jpg|png|svg|gif)$/,
+            type: 'asset/resource',
+      },
+    ],
   },
+  resolve: {
+      extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  }, 
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './src/index.html'),
+        filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+}
