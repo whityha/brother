4a54969460ada049b27322e4b9edde6f303905ea
Pavel <2042_2042@mail.ru>
2022-07-09 11:03:39 +0300

    feat: added the search-area with status

--- online-store/src/components/view/search/search-area.ts
+++ online-store/src/components/view/search/search-area.ts
@@ -1,12 +1,32 @@
 import './search-area.sass';
 //asd
 export default class SearchArea {
+    private events(input: HTMLInputElement, closeBtn: HTMLButtonElement) {
+        input.addEventListener('input', (/*e todo rename*/) => {
+            if (input.value.length && !closeBtn.classList.contains('close')) closeBtn.classList.add('close');
+            else if (!input.value.length) closeBtn.classList.remove('close');
+        });
+        closeBtn.addEventListener('click', () => {
+            input.value = '';
+            closeBtn.classList.remove('close');
+            input.focus();
+        });
+    }
+
     render() {
         const searchArea = document.querySelector('.search-area') as HTMLDivElement;
         const input = document.createElement('input') as HTMLInputElement;
+        const closeBtn = document.createElement('button') as HTMLButtonElement;
+
         input.setAttribute('placeholder', 'Найдите свой любимый чай');
         input.setAttribute('type', 'text');
+        input.setAttribute('autofocus', 'true');
+
         input.className = 'search-input';
-        searchArea.append(input);
+        closeBtn.className = 'search-icon';
+
+        this.events(input, closeBtn);
+
+        searchArea.append(closeBtn, input);
     }
 }
